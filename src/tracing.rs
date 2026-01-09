8e2bab7343de9b7f039e07612cb7912b8c20376c
Lilith Silver <84940819+LilithSilver@users.noreply.github.com>
2026-01-09 09:08:01 -0800

    Set explicit tracing (maybe slow)

--- src/tracing.rs
+++ src/tracing.rs
@@ -4,8 +4,8 @@ pub fn initialize_tracing() {
     let stdout_layer = tracing_subscriber::fmt::layer()
         .compact()
         .with_filter(EnvFilter::new("info")
-            .add_directive("samod=info".parse().unwrap())
-            .add_directive("samod_core=info".parse().unwrap()));
+            .add_directive("samod=trace".parse().unwrap())
+            .add_directive("samod_core=trace".parse().unwrap()));
         
     if let Err(e) = tracing_subscriber::registry()
         .with(stdout_layer)
